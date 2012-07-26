95c1fccb88a884813add9a88717bd36e65fd5f6d
takezoe <takezoe@gmail.com>
2012-07-27 01:34:34 +0900

    Removed configuration for private network.

--- sbt.bat
+++ sbt.bat
@@ -1,3 +1,3 @@
 @echo off
 set SCRIPT_DIR=%~dp0
-java -Dhttp.proxyHost=proxy.intellilink.co.jp -Dhttp.proxyPort=8080 -Xmx512M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
+java -Xmx512M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
