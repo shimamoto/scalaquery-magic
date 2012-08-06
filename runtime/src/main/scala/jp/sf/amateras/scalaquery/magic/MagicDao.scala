c6ce96d53d00d6e33083e5e9c5336342657173f9
shimamoto <chibochibo@gmail.com>
2012-08-06 22:54:21 +0900

    build query by bind variables.

--- runtime/src/main/scala/jp/sf/amateras/scalaquery/magic/MagicDao.scala
+++ runtime/src/main/scala/jp/sf/amateras/scalaquery/magic/MagicDao.scala
@@ -30,15 +30,7 @@ trait MagicDao[R <: GenericEntity] {
    * @return an option value containing the first row
    */
   def selectById(id: table.PK): Option[R] = {
-    implicit val f = addBindVariables	// build query by bind variables
-
-    val sql = table.where { t =>
-      id match {
-        case v: Product => setConditions(t.PKColumn.nodeChildren, v)
-        case v => setCondition(t.PKColumn.nodeDelegate, v)
-      }
-    }
-    sql(id).firstOption
+    sql(id) { _.firstOption }
   }
 
   /**
@@ -58,15 +50,7 @@ trait MagicDao[R <: GenericEntity] {
    * @return results row count
    */
   def update(o: R): Int = {
-    implicit val f = addVariables	// compiled with every execution
-
-    val sql = table.where { t =>
-      o.PKValue match {
-        case v: Product => setConditions(t.PKColumn.nodeChildren, v)
-        case v => setCondition(t.PKColumn.nodeDelegate, v)
-      }
-    }.map(_.**)
-    sql.update(o)
+    sql(o.PKValue) { _.map(_.**).update(o) }
   }
 
   /**
@@ -76,16 +60,22 @@ trait MagicDao[R <: GenericEntity] {
    * @return results row count
    */
   def deleteById(id: table.PK): Int = {
-    implicit val f = addVariables	// compiled with every execution
+    sql(id) { _.delete }
+  }
 
-    val sql = table.where { t =>
+  /**
+   * Build and execute SQL.
+   * 
+   * @param id primary key
+   * @param f a function which execute SQL
+   */
+  private def sql[A](id: Any)(f: Query[ExtendedTable[R]] => A) =
+    f(table.where { t =>
       id match {
         case v: Product => setConditions(t.PKColumn.nodeChildren, v)
         case v => setCondition(t.PKColumn.nodeDelegate, v)
       }
-    }
-    sql.delete
-  }
+    })
 
   /**
    * Build where condition of multiple primary key.
@@ -94,31 +84,29 @@ trait MagicDao[R <: GenericEntity] {
    * @param id primary key
    * @param cond built conditions
    * @param i current primary key position
-   * @param add a function which build each condition
    */
   @tailrec
   private def setConditions(
       cols :List[Node],
       id: Product,
       cond: Column[Boolean] = null,
-      i: Int = 0)(implicit add: (Any, ColumnBase[_]) => Column[Boolean]): Column[Boolean] =
+      i: Int = 0): Column[Boolean] =
     if(cols.isEmpty) cond
     else if(cond == null) setConditions(cols.tail, id,
-        add(id.productElement(i), cols.head.asInstanceOf[ColumnBase[_]]), i + 1)
+        addBindVariables(id.productElement(i), cols.head.asInstanceOf[ColumnBase[_]]), i + 1)
     else setConditions(cols.tail, id,
-        cond && add(id.productElement(i), cols.head.asInstanceOf[ColumnBase[_]]), i + 1)
+        cond && addBindVariables(id.productElement(i), cols.head.asInstanceOf[ColumnBase[_]]), i + 1)
 
   /**
    * Build where condition of single primary key.
    * 
    * @param col [[org.scalaquery.ql.Column]] object of primary key
    * @param id primary key
-   * @param add a function which build single condition
    */
   private def setCondition(
       col: Node,
-      id: Any)(implicit add: (Any, ColumnBase[_]) => Column[Boolean]): Column[Boolean] =
-    add(id, col.asInstanceOf[ColumnBase[_]])
+      id: Any): Column[Boolean] =
+    addBindVariables(id, col.asInstanceOf[ColumnBase[_]])
 
   /**
    * The function which build query by bind variables.
@@ -127,14 +115,5 @@ trait MagicDao[R <: GenericEntity] {
     element match {
       case v: Int => col.asInstanceOf[Column[Int]] is v.bind
       case v: String => col.asInstanceOf[Column[String]] is v.bind
-    }
-  /**
-   * The function which compiled with every execution.
-   * See https://github.com/slick/slick/issues/17
-   */
-  private val addVariables = (element: Any, col: ColumnBase[_]) =>
-    element match {
-      case v: Int => col.asInstanceOf[Column[Int]] is v
-      case v: String => col.asInstanceOf[Column[String]] is v
     }
 }
