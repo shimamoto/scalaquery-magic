ee72c83c72cda2782f18ea9f59e39ac021212b02
shimamoto <chibochibo@gmail.com>
2012-09-18 22:40:15 +0900

    Add contains method for Option column.

--- runtime/src/main/scala/jp/sf/amateras/scalaquery/magic/ColumnOps.scala
+++ runtime/src/main/scala/jp/sf/amateras/scalaquery/magic/ColumnOps.scala
@@ -9,8 +9,13 @@ trait ColumnOps {
   /**
    * ambiguous retrieval.
    */
-  implicit def columnOpsContains(column: NamedColumn[String]) = new {
+  implicit def valueContains(column: NamedColumn[String]) = new {
     def contains[R](s: String)(implicit om: OptionMapper2[String, String, Boolean, String, String, R]): Column[R] =
+      om(ColumnOps.Like(column, ConstColumn('%' + ColumnOps.likeEncode(s) + '%'), Some('^')))
+  }
+
+  implicit def optionValueContains(column: NamedColumn[Option[String]]) = new {
+    def contains[R](s: String)(implicit om: OptionMapper2[String, String, Boolean, Option[String], Option[String], R]): Column[R] =
       om(ColumnOps.Like(column, ConstColumn('%' + ColumnOps.likeEncode(s) + '%'), Some('^')))
   }
 
